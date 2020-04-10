412ab0f7855604ae56e42a92830112f8838ddfa1
caseyhunt <34079427+caseyhunt@users.noreply.github.com>
2020-04-09 19:23:14 -0600

    I'm done for tonight
    I added a section trying to copy files over to a new directory if they have a number of pictures equal to 1. I think it's line 52 where I'm running into trouble. We might want to try a new library?

--- trying open_cv/facedetectionrectloopingfiles.py
+++ trying open_cv/facedetectionrectloopingfiles.py
@@ -9,16 +9,18 @@ Created on Mon Apr  6 14:33:08 2020
 import numpy as np
 import cv2
 import os
-
+import shutil
 
 photo_array = []
+directory = os.path.dirname(os.path.abspath(__file__))
+source = directory + "/images"
+destination = directory + "/images_2"
 
 """
 Gets all image names from specified directory
 """
 def get_images_from_directory():
-    directory = os.path.dirname(os.path.abspath(__file__))
-    for filename in os.listdir(directory+"/images"):
+    for filename in os.listdir(source):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             photo_array.append(filename)
         else:
@@ -30,36 +32,37 @@ def get_images_from_directory():
 computer vision stuff we need to flush through :)
 TODO: identify only one face
 """
-def compVis():
-    image = "images/" + input('what is the name of your image? ') 
+def compVis(pic):
+    image = "images/" + pic
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
     img = cv2.imread(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-    if faces == ():
-        print('no face')
-    else:
-        print(len(faces))
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    print(photo_array[0])
+    numFaces = len(faces)
+    print("image name ", pic, " number of faces= ", numFaces)
+    return(numFaces)
 
+"""copy file to new directory if there is one face in the picture (I haven't gotten it to work yet...)"""
+def copyToDir(origDir):
+    for picture in origDir:
+        if compVis(picture) == 1:
+            fileSrc = "images/" + picture
+            fileDest = directory + "/test/" + picture
+            shutil.copyfile(fileSrc, fileDest)
+        print("files copied successfully")
+            
+            
+        
 """
 Main function
 """
 def main():
-    compVis()
+    compVis("1.jpg")
     get_images_from_directory()
     print(np.array(photo_array))
+    #copyToDir(photo_array)
 
 if __name__== "__main__":
     main()
