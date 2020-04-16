1b1d2808d21c50deb34d4f07164fd659cb222620
caseyhunt <34079427+caseyhunt@users.noreply.github.com>
2020-04-16 17:24:37 -0600

    Cleaned it up a lot :)

--- trying open_cv/FaceDetection_JSON_Connection.py
+++ trying open_cv/FaceDetection_JSON_Connection.py
@@ -20,12 +20,13 @@ exclude = []
 include_only = []
 num_face = []
 directory = os.path.dirname(os.path.abspath(__file__))
-#this is where your source goes
-source = directory + "/puertorican/"
-#this is where your destination goes
+#this is where your hashtag name goes (source directory should be named hashtag)
+hashtag_name = "puertorican"
+source = directory + "/" + hashtag_name + "/"
+#this is where your destination directory goes for processed images
 destination = directory + "/images_2"
 #this is the location of the json file
-jsonFile = directory + "/puertorican/puertorican.json"
+jsonFile = directory + "/" + hashtag_name + "/" + hashtag_name + ".json"
 
 
 """function returns JSON file contents"""
@@ -33,15 +34,6 @@ def openJSON(fileName):
     with open(fileName,'r',encoding='utf-8') as j:
         return json.load(j)
         print('JSON Successfully Loaded')
-        #print(data)
-        #print(url)
-        
-        #print(data['GraphImages'][0]['urls'])
-        
-def findPicName(dirName, dirLoc):
-    url = dirName['GraphImages'][dirLoc]['display_url']
-    picName = re.split('[/?]', url)[6]
-    print(picName)
 
 """populate tags_array with filename and tag list"""
 def mkTagArr(json_data):
@@ -54,45 +46,13 @@ def mkTagArr(json_data):
                 tag_array.append([picName, tags])
             else:
                 continue
-        
-        
-    
-
-"""
-Gets all image names from specified directory
-"""
-def get_images_from_directory():
-    for filename in os.listdir(source):
-        if filename.endswith(".jpg") or filename.endswith(".png"):
-            photo_array.append(filename)
-        else:
-            continue
-        
-        
-        
+              
 """
 computer vision stuff we need to flush through :)
-TODO: identify only one face
+TODO: parse based on included and excluded hashtags
 """
 def compVis(pic):
-    image = "puertorican/" + pic
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    img = cv2.imread(image)
-    if type(img)!='NoneType':
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        numFaces = len(faces)
-        print("image name ", pic, " number of faces= ", numFaces)
-        return(numFaces)
-    else:
-        print("image name ", pic, " not available in directory")
-        print(type(img))
-        
-        
-def compVis2(pic):
-    image = "puertorican/" + pic
+    image = hashtag_name + "/" + pic
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     img = cv2.imread(image)
     if isinstance(img, np.ndarray):
@@ -109,53 +69,30 @@ def compVis2(pic):
         
 
 """copy file to new directory if there is one face in the picture
-Enter your source and destination in variable definitions above"""
-def copyToDir(origDir):
-    for picture in origDir:
-        if compVis(picture) == 1:
-            fileSrc = source + picture
-            fileDest = destination
-            shutil.copy(fileSrc, fileDest)
-            print(picture, "copied successfully")
-        else:
-            print(picture, "not copied")
+!Enter your source and destination in variable definitions above"""
             
 def copyPic(origDir):
     for x in range (0, len(origDir)):
-        if compVis2(origDir[x][0]) == 1:
+        if compVis(origDir[x][0]) == 1:
             fileSrc = source + origDir[x][0]
             fileDest = destination
             shutil.copy(fileSrc, fileDest)
             print(origDir[x][0], "copied successfully")
         else:
             print(origDir[x][0], "not copied")
-
-def numFaces(origDir):
-    for x in range (0, len(origDir)):
-        if origDir[x][0].endswith('.jpg'):
-            num_face.append([origDir[x][0], compVis(origDir[x][0])])
-        else:
-            continue
             
         
 """
 Main function
 """
 def main():
-  
-    get_images_from_directory()
-    #print(np.array(photo_array))
-    #copyToDir(photo_array)
-    #openJSON(jsonFile)
     mkTagArr(openJSON(jsonFile))
     print(tag_array)
-    compVis2(tag_array[6][0])
+    compVis(tag_array[6][0])
     print(tag_array[4][0])
     print(len(tag_array))
-    #numFaces(tag_array)
-    #print(num_face)
     copyPic(tag_array)
-    #findPicName(openJSON(jsonFile), 0)
+
 
 if __name__== "__main__":
     main()
